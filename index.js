e41649d0439531134878d0ed26bd4845fcb75965
tai2 <hello@tai2.net>
2023-09-25 22:33:36 +0900

    Initial import

---
+++ index.js
@@ -0,0 +1,37 @@
+const wdio = require("webdriverio")
+const fs = require("fs")
+
+const opts = {
+  path: '/wd/hub',
+  port: 4723,
+  capabilities: {
+    'platformName': "ios",
+    "appium:automationName": "XCUITest",
+    'appium:platformVersion': "15.2",
+    'appium:deviceName': "iPhone 12 Pro",
+    'appium:app': "http://localhost:8081/UIKitCatalog-iphonesimulator.app.zip",
+    'appium:language': "en",
+    'appium:orientation': "PORTRAIT",
+    //'appium:noReset': true,
+    'appium:processArguments': {},
+    'appium:clearSystemFiles': false,
+    'appium:skipLogCapture': false,
+  }
+}
+
+async function main () {
+  const client = await wdio.remote(opts)
+
+  await client.execute("mobile:configureLocalization", {
+    keyboard: {
+        name: "ja_JP-Kana",
+        layout: "Kana",
+        hardware: "Automatic"
+    }
+  })
+  
+  const windowSize = await client.getWindowSize()
+  console.log(windowSize)
+}
+
+main()
